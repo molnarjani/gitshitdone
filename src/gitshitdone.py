e20ad40fe437f6354694ed50be6df049588be100
Janos Molnar <janosmolnar1001@gmail.com>
2019-12-02 22:44:05 +0100

    feat: Add command to show buffer for the boards

--- src/gitshitdone.py
+++ src/gitshitdone.py
@@ -1,4 +1,8 @@
 import vim
+import inspect
 
-def insert_text():
-    vim.current.buffer.append('HI there little vim user')
+def show_boards():
+    win_width = vim.current.window.width
+    one_third = win_width // 3
+    vim.command(':vsp Boards')
+    vim.command(f':vertical resize {one_third}')
